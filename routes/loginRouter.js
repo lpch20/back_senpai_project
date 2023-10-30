b011ed3db43240480a232f63196562ccb8f0ac56
Ignacio Lopez <nacholopez18@hotmail.com>
2023-10-30 00:18:59 -0300

    cambios de profile

--- routes/loginRouter.js
+++ routes/loginRouter.js
@@ -1,10 +1,16 @@
 const express = require("express");
-const { loginMail, loginUser, loginPassword } = require("../controllers/loginController");
+const {
+  loginMail,
+  loginUser,
+  loginPassword,
+  userData,
+} = require("../controllers/loginController");
 
 const router = express.Router();
 
-router.get("/verifyDataMail", loginMail)
-router.get("/verifyDataUser", loginUser)
-router.post("/verifyDataPassword", loginPassword)
+router.get("/verifyDataMail", loginMail);
+router.get("/verifyDataUser", loginUser);
+router.post("/verifyDataPassword", loginPassword);
+router.get("/dataUser", userData);
 
 module.exports = router;
